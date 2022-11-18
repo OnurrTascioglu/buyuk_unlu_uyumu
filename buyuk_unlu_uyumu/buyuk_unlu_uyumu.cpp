d56f26bc3b1c51124a3bb57b6c9d63876a0ff57c
Onur Tascioglu <onurtsc@outlook.com>
2022-11-18 19:38:21 +0300

    Modification

--- buyuk_unlu_uyumu/buyuk_unlu_uyumu.cpp
+++ buyuk_unlu_uyumu/buyuk_unlu_uyumu.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include<locale.h> 
+#include <locale.h> 
 
-
-int getStringSize(char* word) {
+int getStringSize(wchar_t* word) {
     int string_size = 0;
     while (*word != '\0') {
         string_size++;
@@ -12,35 +11,35 @@ int getStringSize(char* word) {
 }
 
 
-bool stringCompare(char* word, const char* character_array,int string_size, int char_arr_size) {
-
+bool stringCompare(wchar_t* word, const wchar_t* character_array, int string_size, int char_arr_size) {
     for (int i = 0; i < string_size; i++) {
         for (int j = 0; j < char_arr_size; j++) {
-            if (word[i] == character_array[j])
+            if (int(word[i]) == int(character_array[j])) {
                 return false;
+            }
         }
     }
     return true;
 }
 
-bool vowelHarmony(char* word){
-    const char bold_vowels[] = "aýou";
-    const char thin_vowels[] = "eiöü";
+bool vowelHarmony(wchar_t* word) {
+    const wchar_t bold_vowels[] = L"aýou";   // I 
+    const wchar_t thin_vowels[] = L"eiöü";
 
     int string_size = getStringSize(word);
     bool result = false;
     int j = 0;
 
     for (int i = 0; i < string_size; i++) {
-        for(int j = 0; j < 4 ; j++)              // 4 = kalin_harfler, ince_harfler dizisi boyutu
-           if (word[i] == bold_vowels[j]) {
-               result = stringCompare(word, thin_vowels, string_size, 4);
-               break;
-           }
-           else if (word[i] == thin_vowels[j]) {
-               result = stringCompare(word, bold_vowels, string_size, 4);
-               break;
-           }
+        for (int j = 0; j < 4; j++)              // 4 = kalin_harfler, ince_harfler dizisi boyutu
+            if (word[i] == bold_vowels[j]) {
+                result = stringCompare(word, thin_vowels, string_size, 4);
+                return result;
+            }
+            else if (word[i] == thin_vowels[j]) {
+                result = stringCompare(word, bold_vowels, string_size, 4);
+                return result;
+            }
     }
 
     return result;
@@ -49,16 +48,16 @@ bool vowelHarmony(char* word){
 int main()
 {
     setlocale(LC_ALL, "Turkish");
+    wchar_t word[] = L"kýrlangýç";
 
-    char word[] = "alfabe";     // kelime ç harfi içeriyorsa bazý derleyicilerde sonuç hatalý olabiliyor.
     bool result = vowelHarmony(word);
     if (result == true) {
-        std::cout << word << " buyuk unlu uyumuna uyar";
+        std::cout << " buyuk unlu uyumuna uyar";
     }
     else {
-        std::cout << word << " buyuk unlu uyumuna uymaz";
+        std::cout << " buyuk unlu uyumuna uymaz";
     }
-    
+     
     return 0;
 }
 
